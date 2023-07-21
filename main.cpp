0ebd28382a4259fa0f10171160c4e8caf4edd501
Jojox110 <jonathansamuel.leger@gmail.com>
2023-07-20 21:52:45 -0400

    Removed public DB info from main.cpp

--- main.cpp
+++ main.cpp
@@ -10,14 +10,6 @@ using namespace pqxx;
 
 int main(int argc, char *argv[])
 {
-
-    connection c ("dbname = postgres user = postgres password = 859RHU6A9hUWSimi host = db.wvokvvnurrnibmbzpsff.supabase.co port = 5432");
-
-    std::string sql = "SELECT name FROM Items";
-
-    nontransaction N(c);
-    result R(N.exec(sql));
-
     StoreItem banana = StoreItem(1,0.89,4.7,"Banana","Food","Fruit");
     QString bananaName = banana.getName();
     qInfo()<< bananaName;
